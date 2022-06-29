ec2296084b0dbf95ce637cb3cee8b34c72d84c82
edison gomez jose <kcerostudios@gmail.com>
2022-06-29 16:21:41 -0400

    conclusion Ejercicio 4

--- src/components/ListadoTareas.jsx
+++ src/components/ListadoTareas.jsx
@@ -1,10 +1,49 @@
 import Tarea from './Tarea'
 import TareaForm from './TareaForm'
+import { useState } from 'react'
 function ListaDeTareas (){
+    const [tareas, setTareas] = useState([])
+    const agregarTarea = tarea => {
+        if(tarea.texto.trim()){
+            tarea.texto = tarea.texto.trim();
+            const tareasActualizadas = [tarea, ...tareas]
+            setTareas(tareasActualizadas)
+        }
+    }
+    const eliminarTarea = id => {
+        const tareasActualizadas = tareas.filter(tarea => tarea.id !== id)
+        setTareas(tareasActualizadas)
+    }
+    const completarTarea = id => {
+        const tareasActualizadas = tareas.map(tarea => {
+            if (tarea.id === id){
+                tarea.completada = !tarea.completada;
+            }
+            return tarea;
+        });
+        setTareas(tareasActualizadas);
+    }
+
     return(
         <>
-            <TareaForm />
-            <Tarea texto='Aprender react' />
+            <TareaForm onSubmit={agregarTarea} />
+            <div className='tareasListaContenedor'>
+                {
+                    tareas.map((tarea)=>
+                    <Tarea
+                        key={tarea.id}
+                        id={tarea.id}
+                        texto={tarea.texto}
+                        completada={tarea.completada}
+                        completarTarea={completarTarea}
+                        eliminarTarea={eliminarTarea}
+                    /> )
+                    
+                    
+                    
+                }
+            </div>
+        
         </>
     )
 }
--- src/components/TareaForm.jsx
+++ src/components/TareaForm.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
+import {v4 as uuidv4} from 'uuid'
+
 function TareaForm (props){
+    const [input, setInput] = useState('')
+    const manejarCambio = e => {
+        setInput(e.target.value)
+    }
+    const manejarEnvio = e =>{
+        e.preventDefault()
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: input,
+            completada: false,
+        }
+
+        props.onSubmit(tareaNueva)
+    }
     return(
-        <div className="tareaForm">
+        <form 
+            className="tareaForm"
+            onSubmit={manejarEnvio}
+        >
             <input 
-            type="text" 
-            placeholder="Escribe una Tarea"/>
+                className="tareaInput"
+                name="texto"
+                type="text" 
+                placeholder="Escribe una Tarea"
+                onChange={manejarCambio}
+            />
             <button>Agregar Tarea</button>
-        </div>
+        </form>
     )
 
 }
